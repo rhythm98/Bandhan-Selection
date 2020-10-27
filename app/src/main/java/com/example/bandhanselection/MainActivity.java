a51c7988edfb23c9bf26e55afb044d86b0d577dd
rhythm98 <rhyagr@gmail.com>
2020-10-27 19:58:36 +0530

    Version Updated

--- app/src/main/java/com/example/bandhanselection/MainActivity.java
+++ app/src/main/java/com/example/bandhanselection/MainActivity.java
@@ -21,36 +21,51 @@ import androidx.navigation.ui.NavigationUI;
 
 import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+    /*private DrawerLayout drawer;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private AppBarConfiguration appBarConfiguration;*/
+
+    private NavigationView navigationView;
     private DrawerLayout drawer;
     private ActionBarDrawerToggle actionBarDrawerToggle;
-    private AppBarConfiguration appBarConfiguration;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView=findViewById(R.id.nav_view);
         drawer = findViewById(R.id.drawer_layout);
-        Toolbar toolbar = findViewById(R.id.topAppBar);
+        toolbar = findViewById(R.id.topAppBar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.open_nav_desc,R.string.close_nav_desc);
+
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        /*getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);*/
+        /*toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("Rajaji", "onClick: 123456789");
                 Toast.makeText(getApplicationContext(),"Mujhe maaf karna",Toast.LENGTH_SHORT).show();
             }
-        });
-        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.open_nav_desc,R.string.close_nav_desc);
-        drawer.addDrawerListener(actionBarDrawerToggle);
-    navigationView.setNavigationItemSelectedListener(this);
+        });*/
+//        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.open_nav_desc,R.string.close_nav_desc);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+
+        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
 
         NavigationUI.setupWithNavController(
-                toolbar, navController, appBarConfiguration);
+                toolbar, navController, appBarConfiguration);*/
 
 /*
         navigationView.setNavigationItemSelectedListener(onNavigationItemSelected());
@@ -117,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     */
     }
 
-
+/*
     @Override
     public boolean onSupportNavigateUp() {
         Log.d("ajit", "onSupportNavigateUp: ");
@@ -126,7 +141,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-
+*/
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
@@ -148,11 +163,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        actionBarDrawerToggle.syncState();
-    }
+
 
     @Override
     public void onBackPressed() {
